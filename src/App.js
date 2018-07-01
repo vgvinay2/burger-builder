5cafb7bf804686432f87fd40bb550dd0f2310802
Vgupta <vinay.gupta.contractor@move.com>
2018-07-01 14:49:46 +0530

    Remove unwanted Code & file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      HEY Ready setup
       </div>
     );
   }
