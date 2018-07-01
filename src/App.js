6142b6345c6529b7eac1f63acec938e7500f5b25
Vgupta <vinay.gupta.contractor@move.com>
2018-07-01 15:35:59 +0530

    Higher order component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      HEY Ready setup
+        <Layout>
+          <p> TEST </p>
+        </Layout>
       </div>
     );
   }
